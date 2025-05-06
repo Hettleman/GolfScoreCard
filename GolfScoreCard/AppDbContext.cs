b9589b1ca8c854dbe5032ab9b2a921c36404c178
jsanderswp <jsanders3141@icloud.com>
2025-05-06 15:07:38 -0700

    changed connection string

--- GolfScoreCard/AppDbContext.cs
+++ GolfScoreCard/AppDbContext.cs
@@ -8,5 +8,6 @@ public class AppDbContext : DbContext
     public DbSet<Score> Scores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer("Server=tcp:parpalserver.database.windows.net,1433;Initial Catalog=parpaldb;Persist Security Info=False;User ID=CloudSA63016971;Password=rexzis-piPwip-9xidvu;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => options.UseSqlServer(
+            "Server=tcp:parpalserver.database.windows.net,1433;Initial Catalog=parpaldb;Persist Security Info=False;User ID=jsanderswp;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
 }

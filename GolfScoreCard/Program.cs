90f03c94c3c1cf794463b48684395ef016ae53d3
RayHett <rhettleman@chapman.edu>
2025-05-06 17:46:35 -0700

    Merged all changes

--- GolfScoreCard/Program.cs
+++ GolfScoreCard/Program.cs
@@ -1,10 +1,12 @@
 using GolfScoreCard;
+using GolfScoreCard.APISTUFF;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<FetchFromAPI>();
 
 var connectionString = "Server=tcp:parpalserver.database.windows.net,1433;Initial Catalog=parpaldb;Persist Security Info=False;User ID=jsanderswp;Password=vckzKL#k;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
@@ -25,4 +27,14 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
+
+app.MapGet("/api/golfcourses", async (string query, FetchFromAPI fetcher) =>
+{
+    if (string.IsNullOrWhiteSpace(query))
+        return Results.BadRequest("Missing query");
+ 
+    var json = await fetcher.SearchCoursesAsync(query);
+    return Results.Content(json, "application/json");
+});
+
 app.Run();

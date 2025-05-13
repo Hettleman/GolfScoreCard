a02e767e489ce07f60d26f34bed6545cdec763a0
RayHett <rhettleman@chapman.edu>
2025-05-13 15:36:30 -0700

    Scorecard UI added

---
+++ GolfScoreCard/Pages/Scorecard.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GolfScoreCard.Pages.Shared;
+
+public class ScorecardModel : PageModel
+{
+    // services
+
+    public void OnGet()
+    {
+        // initial data
+    }
+}
